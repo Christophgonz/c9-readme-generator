6f6d65fb56f3294be78915eb70fcdd0af3e24cac
cgonz <christoph.gonz@gmail.com>
2023-04-22 21:06:25 -0700

    implemented inquirer prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -16,7 +17,60 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: questions[0],
+    },
+    {
+      type: "input",
+      name: "description",
+      message: questions[1],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions[3],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: questions[4],
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: questions[5],
+    },
+    {
+      type: "list",
+      choices: [
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+      ],
+      name: "license",
+      message: "Choose a license",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your GitHub username",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address",
+    },
+  ]);
+}
 
 // Function call to initialize app
 init();
