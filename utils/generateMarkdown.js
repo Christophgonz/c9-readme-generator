18e74ae584f2cef9390219d5ca70ce1a6829df33
cgonz <christoph.gonz@gmail.com>
2023-04-24 17:55:49 -0700

    edited the link to the users github

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -94,7 +94,7 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## Questions
-  Check out my work at github.com/${data.username}\n
+  Check out my work at https://github.com/${data.username}\n
   If you have further questions, you can contact me at ${data.email}
 
 `;
